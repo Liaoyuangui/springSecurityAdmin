f6aef138e2a1cc614fc03e2db13c33d1c624471b
Liaoyuangui <1129896968@qq.com>
2023-03-30 19:14:59 +0800

    删除多余的备份文件

--- src/main/java/com/example/springsecurity/security/controller/SysLoginController.java
+++ src/main/java/com/example/springsecurity/security/controller/SysLoginController.java
@@ -7,6 +7,29 @@ import com.example.springsecurity.system.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+
+/**
+ 常用的注解：
+ @PreAuthorize 进入方法前的权限验证，如@PreAuthorize("hasAuthority('manager')")
+ @PostAuthorize 使用频率并不高，在方法执行之后再进行权限验证
+ 基于角色和权限进行访问控制
+ Spring Security提供了四个方法用于角色和权限的访问控制。
+ 通过这些方法，对用户是否具有某个或某些权限，进行过滤访问。
+ 对用户是否具备某个或某些角色，进行过滤访问。
+ 1.hasAuthority方法
+ 判断当前主题是否有指定的权限，有返回true，否则返回false
+ 该方法适用于只拥有一个权限的用户。
+ 2.hasAnyAuthority方法
+ 适用于一个主体有多个权限的情况，多个权限用逗号隔开。
+ 3.hasRole方法
+ 如果用户具备给定角色就允许访问，否则报403错误。
+ hasRole需要在设置时加上“ROLE_”前缀，因为通过源码hasRole方法给自定义的角色名前加上了“ROLE_”前缀
+ 4.hasAnyRole方法
+ 设置多个角色，多个角色之间使用逗号隔开，只要用户具有某一个角色，就能访问。
+ **/
+
+
+
 /**
  * @Auther: Liaoyuangui
  * @Date: 2023/3/8 15:30
